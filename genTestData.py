496040d89205bbc864b3980265a5209fe2968345
Christine Corbett Moran <christine.corbett@gmail.com>
2014-07-26 17:35:25 -1000

    fingerprint list to test generation script completed

--- genTestData.py
+++ genTestData.py
@@ -1,32 +1,24 @@
 #!/usr/bin/env python
 
+from random import choice
+import sys
+
 from hexadecimal_testdata.hexdata import genData as hexData
 from englishword_testdata.gen_basic import genData as englishWordData
 from englishpoems_testdata.gendata import genData as englishPoemData
 from pseudoword_testdata.keyname_script import genData as pseudoWordData
-from random import choice
 
-"""4 Fingerprint Types (TODO: what happened to 5th?)
+"""
+4 Fingerprint Types (TODO: what happened to 5th?)
 2 Comparison Mechanisms
 2 Error Rates
 x 2 Outcomes (Match or Not-Match)
- ---
- 32 Test Cases
+---
+32 Test Cases
  
- Run: ./genTestData.py > testdata.csv"""
+Run: ./genTestData.py <testerpairs> > testData.csv
+"""
 
-
-
-
-def demo():
-    for f in hexData():
-        print f
-    for f in englishWordData():
-        print f
-    for f in englishPoemData():
-        print f
-    for f in pseudoWordData():
-        print f
 
 def farFingerprintsToCompare(genData) :
     perfectMatch,almostMatch = genData()
@@ -37,27 +29,28 @@ def closeFingerprintsToCompare(genData):
     perfectMatch,almostMatch = genData()
     noMatch,noMatch =  genData()
     return perfectMatch,choice([perfectMatch,almostMatch])
+
+def normalize(s):
+    return s.replace('\n','<br>').strip()
     
 
-
-testerpairs=1
 fingerprints = {'hex':hexData,'english word':englishWordData,'english poem':englishPoemData,'pseudo word':pseudoWordData}
 comparisons = ['Business card','Phone']
-errors = {'large mismatch':farFingerprintsToCompare,'small mismatch':closeFingerprintsToCompare }
+errors = {'large mismatch':farFingerprintsToCompare,'small mismatch':closeFingerprintsToCompare}
 outcomes = ['match','not match']
 
-def genTestData():
+def genTestData(testerpairs):
     print '#pair\tfingerprint\tcomparison\terror\tAlice\tBob\tjudgement'
     for tid in  range(testerpairs):
         for mech in comparisons:
             for errorDesc,errorFunc in errors.items():
                 for fingerprintDesc,fingerprintFunc  in fingerprints.items():
                     for possibleOutcome in outcomes:
-                        alicePrint,bobPrint=errorFunc(fingerprintFunc)
-                        print '\t'.join([str(tid),fingerprintDesc,mech,errorDesc,alicePrint,bobPrint])
+                        alicePrint,bobPrint = errorFunc(fingerprintFunc)
+                        print '\t'.join([str(tid),fingerprintDesc,mech,errorDesc,normalize(alicePrint),normalize(bobPrint)])
 
 
 if __name__ == '__main__':
-    genTestData()
+    genTestData(sys.argv[1])
     
     
